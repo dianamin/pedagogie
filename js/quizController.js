77f87efe9549ba030be3ea015ea7025d8ed582f1
Diana Ghinea <97.diana@gmail.com>
2016-04-30 18:22:07 +0300

    design and functionality

--- js/quizController.js
+++ js/quizController.js
@@ -1,46 +1,76 @@
-app.controller('quizCtrl', function($scope) {
-  $scope.items = [
-    {
-      "question" : "abcd1",
-      "type" : 2,
-      "givenAnswer": 1
-    },
-    {
-      "question" : "abcd2",
-      "type" : 2,
-      "givenAnswer": 1
-    },
-    {
-      "question" : "abcd3",
-      "type" : 2,
-      "givenAnswer": 1
-    },
-    {
-      "question" : "abcd4",
-      "type" : 2,
-      "givenAnswer": 1
+  app.controller('quizCtrl', function($scope) {
+    $scope.items = [
+      {
+        "question" : "abcd1",
+        "type" : 2,
+        "givenAnswer": 1
+      },
+      {
+        "question" : "abcd2",
+        "type" : 2,
+        "givenAnswer": 1
+      },
+      {
+        "question" : "abcd3",
+        "type" : 2,
+        "givenAnswer": 1
+      },
+      {
+        "question" : "abcd4",
+        "type" : 2,
+        "givenAnswer": 1
+      }
+    ];
+
+    $scope.currentItemIndex = 0;
+    $scope.currentItem = $scope.items[0];
+
+    $scope.currentAnswer = 0;
+
+
+    $scope.results = [
+        {
+          "givenCount" : 0,
+          "totalCount" : 0
+        },
+        {
+          "givenCount" : 0,
+          "totalCount" : 0
+        },
+        {
+          "givenCount" : 0,
+          "totalCount" : 0
+        },
+        {
+          "givenCount" : 0,
+          "totalCount" : 0
+        },
+        {
+          "givenCount" : 0,
+          "totalCount" : 0
+        },
+        {
+          "givenCount" : 0,
+          "totalCount" : 0
+        }
+    ];
+    $scope.showResults = false;
+
+    $scope.getResult = function() {
+      for (var i = 0; i < $scope.items.length; i++) {
+        $scope.results[$scope.items[i].type - 1].givenCount += $scope.items[i].givenAnswer;
+        $scope.results[$scope.items[i].type - 1].totalCount++;
+      }
+      $scope.showResults = true;
     }
-  ];
 
-  $scope.currentItemIndex = 0;
-  $scope.currentItem = $scope.items[0];
-
-  $scope.currentAnswer = 0;
-
-  $scope.nextItem = function() {
-    if ($scope.currentItemIndex == $scope.items.length - 1) {
-      alert("sfarsit");
-      return;
+    $scope.nextItem = function() {
+      if ($scope.currentItemIndex == $scope.items.length - 1) {
+        $scope.getResult();
+        return;
+      }
+      $scope.currentItemIndex = $scope.currentItemIndex + 1;
+      $scope.currentItem = $scope.items[$scope.currentItemIndex];
     }
-    $scope.currentItemIndex = $scope.currentItemIndex + 1;
-    $scope.currentItem = $scope.items[$scope.currentItemIndex];
-  }
 
-
-  $scope.results = [0, 0, 0, 0, 0, 0];
-
-  $scope.getResult = function() {
-    for (var i = 0; i < $scope.items.length; i++)
-      $scope.results[$scope.items[i].type] += $scope.intems[i].givenAnswer;
-  }
-});
+  });
